ecf98c0d0255988ac76ba12a485a5a3713d7f17b
Arthur Lage <arthurlage2006@gmail.com>
2023-05-23 14:41:53 -0300

    feat: Allow custom texts in Loading component through props

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -1,6 +1,10 @@
 import { CircleNotch } from "@phosphor-icons/react";
 
-export function Loading() {
+interface LoadingProps {
+  loadingText?: string;
+}
+
+export function Loading({ loadingText }: LoadingProps) {
   return (
     <div className="flex items-center justify-center absolute top-0 left-0 right-0 bottom-0 bg-[rgba(0,0,0,0.5)] z-50">
       <div className="p-4 rounded-lg bg-zinc-100 flex flex-col items-center gap-4">
@@ -9,9 +13,13 @@ export function Loading() {
           weight="bold"
           className="animate-spin text-zinc-900"
         />
-        <span className="font-medium font-poppins text-lg">
-          Loading recipes...
-        </span>
+        {loadingText ? (
+          <span className="font-medium font-poppins text-lg">
+            {loadingText}
+          </span>
+        ) : (
+          ""
+        )}
       </div>
     </div>
   );
