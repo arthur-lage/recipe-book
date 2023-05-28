25718079ce188e3d288263855983544e9a2aae87
Arthur Lage <arthurlage2006@gmail.com>
2023-05-28 17:15:45 -0300

    style: set loading position to fixed

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -6,7 +6,7 @@ interface LoadingProps {
 
 export function Loading({ loadingText }: LoadingProps) {
   return (
-    <div className="flex items-center justify-center absolute top-0 left-0 right-0 bottom-0 bg-[rgba(0,0,0,0.5)] z-50">
+    <div className="flex items-center justify-center fixed top-0 left-0 right-0 bottom-0 bg-[rgba(0,0,0,0.5)] z-50">
       <div className="p-4 rounded-lg bg-zinc-100 flex flex-col items-center gap-4">
         <CircleNotch
           size={42}
