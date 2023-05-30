5a0818841556230453bef855532fcad74878db6f
Arthur Lage <arthurlage2006@gmail.com>
2023-05-30 17:28:38 -0300

    feat: return favorite recipes with details

--- src/services/favoriteRecipesService.ts
+++ src/services/favoriteRecipesService.ts
@@ -6,10 +6,22 @@ export const favoriteRecipesService = {
     const favoriteRecipesRef = ref(database, `favoriteRecipes/${userId}`);
 
     const snapshot = await get(favoriteRecipesRef);
-
     const favoriteRecipes = snapshot.val();
 
-    return favoriteRecipes;
+    if (!favoriteRecipes) {
+      return [];
+    }
+
+    const favoriteRecipesIds = Object.keys(favoriteRecipes);
+
+    const recipePromises = favoriteRecipesIds.map((recipeId) => {
+      const recipeRef = ref(database, `recipes/${recipeId}`);
+      return get(recipeRef).then((recipeSnapshot) => recipeSnapshot.val());
+    });
+
+    const favoriteRecipesWithDetails = await Promise.all(recipePromises);
+
+    return favoriteRecipesWithDetails;
   },
   async addRecipeToFavorites(userId: string, recipeId: string) {
     const favoriteRecipesRef = ref(database, `favoriteRecipes/${userId}`);
