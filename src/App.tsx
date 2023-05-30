573d28463d7df159146f127d9b2d11de2178bcd5
Arthur Lage <arthurlage2006@gmail.com>
2023-05-30 17:28:56 -0300

    feat: create route for saved recipes

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { ProtectedRoute } from "./components/ProtectedRoute";
 import { useAuth } from "./hooks/useAuth";
 import { AuthPage, Home, RecipePage, CreateRecipe } from "./pages";
 import { Routes, Route, Navigate } from "react-router-dom";
+import { SavedRecipes } from "./pages/SavedRecipes";
 
 export function App() {
   const { currentUser } = useAuth();
@@ -27,6 +28,14 @@ export function App() {
       />
 
       <Route path="/recipes/new" element={<CreateRecipe />} />
+      <Route
+        path="/recipes/saved"
+        element={
+          <ProtectedRoute>
+            <SavedRecipes />
+          </ProtectedRoute>
+        }
+      />
 
       <Route
         path="/auth"
